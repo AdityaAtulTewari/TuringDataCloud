8b0985b3f4fd594124420120a5c956f19370635c
AdityaAtulTewari <adityaatewari@gmail.com>
2017-12-18 02:08:24 -0600

    Added Red-BlackTree Header file

---
+++ structures/rbtree/rbtree.h
@@ -0,0 +1,51 @@
+//
+// Created by AdityaAtulTewari on 12/18/17.
+//
+
+//Header Guard
+#ifndef RBTREE_H
+#define RBTREE_H
+
+typedef char bool;
+#define true 0x5FFF
+#define false 0x0000
+
+typedef struct rbTreeNode
+{
+  char      color;
+  void*     data;
+  rbNode*   left;
+  rbNode*   right;
+} rbNode;
+
+struct rbTree
+{
+  rbNode*   root;
+}
+
+//Constructs a node assigns it's children to 0 and sets the color to 'r' (red)
+rbNode createRBNode(void* data);
+
+//Destroys a node returning it's data
+void* destructRBNode();
+
+//Construct an empty rbTree
+rbTree* createRBTree();
+
+//Destruct an rbTree
+void destructRBTree(rbTree* tree, void (*destruct)(void*));
+
+//Inserts a node into the red black tree returns null if succeeded, else self
+rbNode* insertRBNode(rbNode* target, rbTree* tree, int (*compare)(void*, void*));
+
+//Deletes a node from the tree, returns rbNode deleted or null if none deleted
+rbNode* deleteRBNode(rbNode* target, rbTree* tree, int (*compare)(void*, void*));
+
+//Finds a node in the tree
+bool findRBNode(rbNode* target, rbTree* tree, int(*compare)(void*, void*));
+
+
+
+#endif
+
+
