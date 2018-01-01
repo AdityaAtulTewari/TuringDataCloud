0056296ca36e1a99586ef128f10dfe94978ae9f6
AdityaAtulTewari <adityaatewari@gmail.com>
2018-01-01 12:59:52 -0600

    Added Testing groundwork

---
+++ tests/rbtree/rbtree_test.cpp
@@ -0,0 +1,9 @@
+//
+// Created by Aditya Tewari on 1/1/18.
+//
+#include "../../structures/rbtree/rbtree.h"
+#define CATCH_CONFIG_MAIN
+#include "../catch.hpp"
+
+int*
+TEST_CASE("Tree Contructor", "[tree]");
